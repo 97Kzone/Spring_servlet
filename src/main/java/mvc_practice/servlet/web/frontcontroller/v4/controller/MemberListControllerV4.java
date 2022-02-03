8dc06e07ea5826f6029854faf0d954d32e5f920c
97Kzone <mmnm3@naver.com>
2022-02-03 21:06:36 +0900

    22.02.03 FrontControllerV4

---
+++ src/main/java/mvc_practice/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,23 @@
+package mvc_practice.servlet.web.frontcontroller.v4.controller;
+
+import mvc_practice.servlet.domain.member.Member;
+import mvc_practice.servlet.domain.member.MemberRepository;
+import mvc_practice.servlet.web.frontcontroller.ModelView;
+import mvc_practice.servlet.web.frontcontroller.v3.ControllerV3;
+import mvc_practice.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members", members);
+        return "members";
+    }
+}
